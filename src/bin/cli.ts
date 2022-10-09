229808758fc57faff7809b603826db4b36a5cfd4
S-codes14 <81442054+S-codes14@users.noreply.github.com>
2022-10-09 18:08:45 +0200

    install methods #1
    there are two methods I can Think of and they are the ones commented

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -13,8 +13,17 @@ const runCommand = (command: string) => {
 };
 
 const projectName = process.argv[2];
+//solution 1 is to have another argument e.g
+// packageManager = process.argv[3]   // where users type in npm or yarn or pnpm but the default will be yarn
+// solution 2 is to actually ask the user what package manager they want to use where users type in npm or yarn or pnpm but the default will be yarn e.g
+// const packageManager = prompt("What package manager do you want to use? ");
+
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/Kazuki-tam/pure-liquid.git ${projectName}`;
+
 const installCommand = `cd ${projectName} && yarn install`;
+// const installCommand = `cd ${projectName} && rm -rf yarn.lock && ${packageManager} install`;
+
+
 
 console.log(`Cloning the repository with name ${projectName}`);
 
